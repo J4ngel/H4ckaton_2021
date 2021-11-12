cf4a59307ffa9826ecff4a141bb2820ccce8b120
J4ngel <andres_angel@live.com.ar>
2021-11-12 04:09:46 -0500

    Se agregó un apartado en constantes para visualisar las alertas (por medio de un flash) se agregaron nuevos metodos a db_manager y verifications, se agregaron los atributos name faltantes a la vista de registro_empleado. Por otro lado en el archivo principal se empezó a utilizar los metodos desarrollados en los otros achivos de python para comenzar a hacer funcinales las paginas de registro

--- db_manager.py
+++ db_manager.py
@@ -4,12 +4,28 @@ from sqlite3 import Error
 import hashlib
 from werkzeug.security import check_password_hash
 
+#----->REGISTRO DE UN USUARIO EXTERNO (CLIENTE)
 def reg_1(cedula, name, gender, birthday, city, adds, phrase, password, rol):
     status={'state':True, 'error':None}
     try:
         with sqlite3.connect("orion.db") as con:
             cur = con.cursor()
             cur.execute("INSERT INTO Usuarios(Cedula, Nombre_y_apellido, Sexo, Fecha_de_nacimiento, Direccion, Ciudad, Contraseña, rol) VALUES (?,?,?,?,?,?,?,?)",(cedula,name,gender,birthday,adds,city,password, rol))
+            con.commit()
+            return status
+            
+    except Error:
+        status['state'] = False
+        status['error'] = "Algo salió mal" + Error
+        return status
+
+#----->REGISTRO DE UN USUARIO INTERNO (EMPLEADO)
+def reg_2(cedula, name, job, gender, birthday, city, adds, phrase, password, rol):
+    status={'state':True, 'error':None}
+    try:
+        with sqlite3.connect("orion.db") as con:
+            cur = con.cursor()
+            cur.execute("INSERT INTO Usuarios(Cedula, Nombre_y_apellido, Sexo, Fecha_de_nacimiento, Direccion, Ciudad, Contraseña, rol, Cargo) VALUES (?,?,?,?,?,?,?,?,?)",(cedula,name,gender,birthday,adds,city,password,rol,job))
             con.commit()
             return status
             
--- main.py
+++ main.py
@@ -27,6 +27,14 @@ def login():
         cedula = escape(request.form['cedula'])
         password = escape(request.form['password'])
         
+        if cedula == "1234" and password=="1234":
+            session['user'] = 1234
+            session['rol'] = 3
+            flash(f"logueado con exito {session['user']}: {session['rol']}")
+            return redirect("/registro/empleado")
+        else:
+            flash("Credenciales invalidas!!")
+            return redirect("/login")
 
     if 'user' in session:
         return redirect('/') #Se redirige al perfil
@@ -55,26 +63,66 @@ def registrarse():
 
         status_1 = verifications.valid_reg_1(cedula,name,gender,birthday,city,adds,phrase,password,check_pass,conditions)
         if not status_1['state']:
-            print(status_1['error'])
+            flash(status_1['error'])
+            return redirect('/registrarse')
         else:
             hash_pass = generate_password_hash(password)
             hash_phrase = generate_password_hash(phrase)
 
-            status_2 = db_manager.reg_1(cedula,name,gender,birthday,city,adds,hash_phrase,hash_pass,rol)
-            
+            #status_2 = db_manager.reg_1(cedula,name,gender,birthday,city,adds,hash_phrase,hash_pass,rol)
+            status_2 = {'state':True} #Linea SOLO PARA PRUEBAS
             if not status_2['state']:
-                print(status_2['error'])
+                flash(status_2['error'])
+                return redirect('/registrarse')
             else:
-                print("Registro exitoso")
+                if 'user' in session and session['rol'] == 3:  
+                    flash("Registro exitoso, nuevo cliente agregado con exito")
+                else:
+                    flash("Registro exitoso, ahora puede dirigirse a login e iniciar sesión")
+                return redirect('/registrarse')
 
-    if 'user' in session:
+    if 'user' in session and session['rol'] != 3:
         return redirect('/')
     else:
         return render_template('registrarse.html')
 
-@app.route('/registro/empleado')
+@app.route('/registro/empleado', methods=['GET', 'POST'])
 def registro_empleado():
-    return render_template('registro_empleado.html')
+    if request.method == 'POST':
+        cedula      = escape(request.form['cedula'])
+        name        = escape(request.form['name'])
+        job         = escape(request.form['job'])
+        gender      = escape(request.form['gender'])
+        birthday     = escape(request.form['birthday'])
+        city        = escape(request.form['city'])
+        adds        = escape(request.form['adds'])
+        phrase      = escape(request.form['phrase'])
+        password    = escape(request.form['password'])
+        check_pass  = escape(request.form['check_pass'])
+        conditions  = escape(request.form.get('conditions'))
+        rol = 2  # 1 -> usuario externo / 2 -> usuario interno / 3 -> admin  
+
+        status_1 = verifications.valid_reg_2(cedula,name,job,gender,birthday,city,adds,phrase,password,check_pass,conditions)
+        if not status_1['state']:
+            flash(status_1['error'])
+            return redirect('/registro/empleado')
+        else:
+            hash_pass = generate_password_hash(password)
+            hash_phrase = generate_password_hash(phrase)
+
+            #status_2 = db_manager.reg_2(cedula,name,job,gender,birthday,city,adds,hash_phrase,hash_pass,rol)
+            status_2 = {'state':True} #Linea SOLO PARA PRUEBAS
+            if not status_2['state']:
+                flash(status_2['error'])
+                return redirect('/registro/empleado')
+            else:
+                flash("Registro exitoso, nuevo empleado agregado con exito")
+                return redirect('/registro/empleado')
+
+    if 'user' in session and session['rol'] != 3:
+        return redirect('/')
+    else:
+        return render_template('registro_empleado.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
